467db8986bca0885e2b90557f9fc6e3840852043
Ashwin Naik <ashwinknaik@google.com>
2021-10-26 14:00:11 +0530

    1

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "google" {
   access_token = var.access_token
 }
+
+data "google_project_organization_policy" "policy" {
+  project    = "ashwinknaik-314910"
+  constraint = "constraints/storage.publicAccessPrevention"
+}
+
+output "version" {
+  value = data.google_project_organization_policy.policy.version
+}
